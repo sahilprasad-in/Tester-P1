5712377d24e8476398ca4c5a705486896c8af405
sahilprasad-in <05sahilprasad@gmail.com>
2024-10-15 17:51:27 +0530

    Updated
    Updated with the latest version and added an option to view the changed value form initial one

--- Tester.sol
+++ Tester.sol
@@ -1,10 +1,13 @@
-pragma solidity >=0.8.4;
+pragma solidity >=0.8.5;
 contract Tester{
     uint256 Coins=0;
     function setValue(uint256 x)public{
         Coins=x;
     }
     function getvalue()public view returns(uint256){
+        return Coins;
+    }
+    function setvalue()public view returns(uint256){
         return Coins;
     }
 }
